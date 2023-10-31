203ad88d1a063790ab770361aa93ded90193afb6
basitali13 <basit.iuic@gmail.com>
2023-10-31 17:11:39 +0500

    changes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import {createRouter, createWebHistory} from 'vue-router';
 import App from './App.vue';
 import TeamsListVue from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+
+//  hello
 const router = createRouter({
     history : createWebHistory(),
     routes:[
